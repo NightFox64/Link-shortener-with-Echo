0cf8aa6548ba18610eb7e8da4172bd21c03f391e
NightFox64 <91380334+NightFox64@users.noreply.github.com>
2024-09-21 21:21:12 +0300

    Add url

--- cmd/linkShortenerEcho/main.go
+++ cmd/linkShortenerEcho/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/NightFox64/Link-shortener-with-Echo/internal/server"
+	"github.com/NightFox64/Link-shortener-with-Echo/internal/shorten"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	url := shorten.Shorten(shorten.GenerateNewURL())
+
 	e := echo.New()
 
 	e.POST("/shorten", server.ShortenURLHandler)
-	e.GET("/:shortened", server.RedirectURLHandler)
+	e.GET("/:shortened", server.RedirectURLHandler(url))
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
